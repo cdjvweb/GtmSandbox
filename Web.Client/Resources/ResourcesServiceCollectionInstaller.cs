9e03bc61b25a3c07577aa7abcd2b9a04a8291513
cdjvweb <70895581+cdjvweb@users.noreply.github.com>
2022-05-13 17:16:18 +0200

    Initial commit

---
+++ Web.Client/Resources/ResourcesServiceCollectionInstaller.cs
@@ -0,0 +1,9 @@
+﻿namespace Havit.NewProjectTemplate.Web.Client.Resources
+{
+	/// <summary>
+	/// Marker file for RESX source generator.
+	/// </summary>
+	public static partial class ResourcesServiceCollectionInstaller
+	{
+	}
+}
